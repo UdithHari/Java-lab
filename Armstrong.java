ab085db31bec7a70cb53da1a066a301a1bc06243
UdithHari <76899284+UdithHari@users.noreply.github.com>
2022-01-28 02:15:54 -0700

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+
+class Armstrong
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		int num,digit=0,sum=0,last=0;
+		
+		System.out.println("Enter number:");
+		num=sc.nextInt();
+		
+		int a=num;
+		while(a!=0)
+		{
+			a/=10;
+			digit++;
+		}
+		a=num;
+		while(a!=0)
+		{
+			last=a%10;
+			sum+=Math.pow(last,digit);
+			a/=10;
+		}
+		
+		if(sum==num)
+		{
+			System.out.println("Armstrong");
+		}
+		else
+		{
+			System.out.println("Not Armstrong");
+		}
+	}
+}
