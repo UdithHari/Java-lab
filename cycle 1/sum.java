c75ed9dc5811d96de6a677e7ea5f48923c4de011
UdithHari <76899284+UdithHari@users.noreply.github.com>
2022-01-28 02:35:23 -0700

    Update and rename Fibonacci.java to cycle 1/sum.java

---
+++ cycle 1/sum.java
@@ -0,0 +1,23 @@
+
+
+import java.util.*;
+
+class sum
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    
+    int n,sum=0;
+    System.out.println("Enter the number:");
+    n=sc.nextInt();
+    
+    for(int i=1;i<=n;i++)
+    {
+      sum+=i;
+    }
+		
+    System.out.println("The sum of first "+n+" natural numbers is:"+sum);
+    
+  }
+}
